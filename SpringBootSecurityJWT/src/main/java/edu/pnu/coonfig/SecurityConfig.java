29812775f2f65aeff3c3ebeba4267dab60d33452
kyung89 <bamtori1029@gmail.com>
2024-05-24 13:09:09 +0900

    2024-05-24 1,2,3,4교시 스프링시큐리티

---
+++ SpringBootSecurityJWT/src/main/java/edu/pnu/coonfig/SecurityConfig.java
@@ -0,0 +1,77 @@
+package edu.pnu.coonfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+import edu.pnu.coonfig.filter.JWTAuthenticationFilter;
+import edu.pnu.coonfig.filter.JWTAuthorizationFilter;
+import edu.pnu.persistance.MemberRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	 private MemberRepository memberRepository;
+	
+	@Autowired
+	 private AuthenticationConfiguration authenticationConfiguration;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf(csrf->csrf.disable());
+
+		http.authorizeHttpRequests(auth->auth
+				.requestMatchers("/member/**").authenticated()
+				.requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll());
+
+
+		http.formLogin(frmLogin->frmLogin.disable());
+		http.httpBasic(basic->basic.disable());
+
+		http.sessionManagement(ssm->ssm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		// 스프링 시큐리티가 등록한 필터체인의 뒤에 작성한 필터를 추가한다.
+		 http.addFilter(new JWTAuthenticationFilter(
+		 authenticationConfiguration.getAuthenticationManager()));
+		 
+		 http.addFilterBefore(new JWTAuthorizationFilter(memberRepository), AuthorizationFilter.class);
+				 
+		return http.build();
+	}
+
+	@Autowired
+	public void authenticate(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		auth.inMemoryAuthentication()
+//		.withUser("manager")
+//		.password("{noop}abcd")
+//		.roles("MANAGER");
+//
+//		auth.inMemoryAuthentication()
+//		.withUser("admin")
+//		.password("{noop}abcd")
+//		.roles("ADMIN");
+		}
+
+
+	 @Bean
+	 PasswordEncoder passwordEncoder() {
+		 return new BCryptPasswordEncoder();
+	 }
+
+}
